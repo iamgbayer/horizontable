35f056bcb841fe386626effbe3c619872e7016a7
guuibayer <guiilherme.bayer@gmail.com>
2019-06-03 10:33:30 -0300

    Improvements on example

--- example/index.js
+++ example/index.js
@@ -1,38 +1,120 @@
 import React from 'react'
 import { render } from 'react-dom'
 import Horizontable from '../src/index'
+import styled, { createGlobalStyle } from 'styled-components'
 
-const style = {
-  parent: {
-    width: '600px',
-    height: '400px',
-    display: 'flex',
-    lineHeight: '400px'
-  },
-  children: {
-    width: '1000px',
-    display: 'flex'
-  },
-  card: {
-    width: '450px',
-    height: '400px',
-    background: 'linear-gradient(to right, #ec6f66, #f3a183)',
-    color: '#fff',
-    fontFamily: 'sans-serif',
-    textAlign: 'center'
+const Title = styled.h2`
+  color: #fff;
+  text-align: center;
+  padding-top: 50px;
+  padding-bottom: 30px;
+  font-weight: 300;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+`
+
+const Description = styled.h4`
+  color: #fff;
+  text-align: center;
+  padding-top: 50px;
+  padding-bottom: 30px;
+  font-weight: 300;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+`
+
+const Card = styled.div`
+  background-color: #f3a183;
+  min-width: 400px;
+  height: 220px;
+  text-align: center;
+  line-height: 220px;
+  margin: 10px;
+  border-radius: 5px;
+  color: #fff;
+`
+
+const Global = createGlobalStyle`
+  * {
+    font-family: 'Lato', sans-serif;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
   }
-}
+`
+
+const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  height: 100%;
+  background-color: #ec6f66;
+  text-align: center;
+`
+
+const FullParent = styled.div`
+  width: 100%;
+
+  .children {
+    display: flex;
+  }
+`
+
+const SpecificParent = styled.div`
+  width: 600px;
+
+  .children {
+    display: flex;
+  }
+`
 
 function Example() {
   return (
-    <div style={style.parent}>
-      <Horizontable>
-        <div style={style.children}>
-          <div style={style.card}>Scroll me 😍</div>
-          <div style={style.card}>Drag me 😍</div>
-        </div>
-      </Horizontable>
-    </div>
+    <>
+      <Global />
+      <Container>
+        <Title>Horizontable, </Title>
+        <iframe
+          className="github"
+          src="https://ghbtns.com/github-btn.html?user=iamgbayer&repo=horizontable&type=star&count=true"
+          frameBorder="0"
+          scrolling="0"
+          width="170px"
+          height="20px"
+        />
+
+        <Description>Parent with full width</Description>
+
+        <FullParent>
+          <Horizontable>
+            <div className="children">
+              <Card>Scroll me 😍</Card>
+              <Card>Drag me 😎</Card>
+              <Card>Scroll me ❤️</Card>
+              <Card>Drag me ✨</Card>
+              <Card>Scroll me 🌀</Card>
+              <Card>Drag me 🌿</Card>
+              <Card>Scroll me 🎉</Card>
+            </div>
+          </Horizontable>
+        </FullParent>
+
+        <Description>Parent with specific width</Description>
+
+        <SpecificParent>
+          <Horizontable>
+            <div className="children">
+              <Card>Scroll me 😍</Card>
+              <Card>Drag me 😎</Card>
+              <Card>Scroll me ❤️</Card>
+              <Card>Drag me ✨</Card>
+              <Card>Scroll me 🌀</Card>
+              <Card>Drag me 🌿</Card>
+              <Card>Scroll me 🎉</Card>
+            </div>
+          </Horizontable>
+        </SpecificParent>
+      </Container>
+    </>
   )
 }
 
